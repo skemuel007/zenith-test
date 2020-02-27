3b5a24d4cc4a9429afbf3ad446d074674e71bb84
skemuel007 <skemuel007@gmail.com>
2020-02-27 11:50:26 +0100

    Scale model updated

--- app/Scale.php
+++ app/Scale.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Scale extends Model
 {
     //
+    protected $fillable = [
+        'monthly',
+        'rate'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }

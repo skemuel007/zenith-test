754baee2cf9026e368465277691213fe1517c263
skemuel007 <skemuel007@gmail.com>
2020-02-27 11:03:16 +0100

    AuthController updated, employee model and migration created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Validator;
 
 class AuthController extends Controller
 {
     //
+    public function __construct()
+    {
+    }
+
+    public function login(Request $request) {
+        // validate request
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required'
+        ]);
+
+        // check for request validation error
+        if( $validator->fails() ) {
+            // return json response with status code 422
+            return response()->json(
+                [
+                    'message' => 'Request validation error',
+                    'error' => $validator->errors()
+                ],
+                JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        // credentials array
+        $credentials = $request->only('email', 'password');
+        // check for credentials and build claims
+        if ( !$token = auth()->attempt($credentials)) {
+            // if credentials not found return json status 401 unauthorized
+            return response()->json([
+                'message' => 'Invalid credentials',
+                'error' => 'Unauthorized'
+            ],
+                JsonResponse::HTTP_UNAUTHORIZED
+            );
+        }
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token
+        ], JsonResponse::HTTP_OK);
+    }
 }

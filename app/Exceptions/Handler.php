23cc2e8a4733abcd679a07887ed6ce3d7960de7a
skemuel007 <skemuel007@gmail.com>
2020-02-27 11:25:58 +0100

    Updated exception instances

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,17 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +60,99 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
-    }
+// this will replace 404 response from mvc to a json response
+        if ( $exception instanceof QueryException) {
+            // if the exception is a Database query exeception
+            return response()->json([
+                'title' => 'Query Error',
+                'message' => $exception->getMessage(),
+                'data' => null
+            ], 500);
+        }
+
+        // This will replace 404 response from the MVC to a JSON response
+        if($exception instanceof ModelNotFoundException && $request->wantsJson())
+        {
+            return response()->json(
+
+                [
+                    'title' => 'Model Exception Error',
+                    'message' =>  $exception->getMessage(),
+                    'data' => null
+                ]
+                , 500);
+        }
+        // https://stackoverflow.com/questions/53279247/laravel-how-to-show-json-when-api-route-is-wrong-or-not-found
+        if($exception instanceof NotFoundHttpException ) {
+            return response()->json([
+                'title' => 'Http Not Found Error',
+                'message' => 'Invalid Http Url - resource locator not found.',
+                'data' => null
+            ], 500);
+        }
+
+        if($exception instanceof NotFoundResourceException){
+            return response()->json([
+                'title' => 'Resource Not Found Error',
+                'message' => 'Resource error',
+                'data' => []
+            ], 500);
+        }
+
+        if($exception instanceof ModelNotFoundException)
+        {
+            return response()->json(
+                [
+                    'title' => 'Model Exception Error',
+                    'message' =>  $exception->getMessage(),
+                    'data' => null
+                ]
+                , 404);
+        }
+
+        if( $exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'title' => 'Http Method Error',
+                'message' => 'Http method not valid for this url',
+                'data' => null ], Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+
+        if ( $exception instanceof BindingResolutionException) {
+            return response()->json([
+                'title' => 'Target Class not found',
+                'message' => $exception->getMessage(),
+                'data' => null
+            ], 500);
+        }
+
+        if( $exception instanceof UnauthorizedHttpException) {
+
+            return response()->json([
+                'title' => 'Authorization Error',
+                'message' => $exception->getMessage(),
+                'data' => ['Either token is expired or not provided']], 401);
+        }
+
+        if( $exception instanceof TokenInvalidException) {
+            return response()->json([
+                'title' => 'Invalid Token Error',
+                'message' => 'Token supplied is invalid, please login with appropriate credentials',
+                'data' => []], 500);
+        }
+
+        if($exception instanceof TokenExpiredException) {
+            return response()->json([
+                'title' => 'Token Expired Error',
+                'message' => 'Login again to get fresh token',
+                'data' => 'Token expired'], 500);
+        }
+
+        // JWT Auth related errors
+        if( $exception instanceof  JWTException) {
+            return response()->json([
+                'title' => 'JWT Exception',
+                'message' => $exception->getMessage(),
+                'data' => []], 500);
+        }
+        return parent::render($request, $exception);    }
 }

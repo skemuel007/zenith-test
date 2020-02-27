b29791f548fbdea891160f00bead29bdd5e1aa10
skemuel007 <skemuel007@gmail.com>
2020-02-27 11:09:24 +0100

    Employee model updated

--- app/Employee.php
+++ app/Employee.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     //
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'salary'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
 }
